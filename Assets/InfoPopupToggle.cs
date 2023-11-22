61911bf9fe349149b4e8dc223e74e43df91fa684
Nikolay Panovski <nikipanovski@gmail.com>
2023-11-22 15:52:54 +0100

    Add info popup object and UI

--- Assets/InfoPopupToggle.cs
+++ Assets/InfoPopupToggle.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InfoPopupToggle : MonoBehaviour
 {
+    public UnityEvent triggerEntered;
+    public UnityEvent triggerExited;
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        triggerEntered.Invoke();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        BillboardAsset asset = new BillboardAsset();
+        triggerExited.Invoke();
     }
 }
